d8d4d50ec7d6ad5692545550cd1f0149cb12ac3a
seiyarick <seiyahirota2@gmail.com>
2022-05-25 17:46:21 +0900

    JavaScriptを学ぼう5章jQuery

---
+++ app.js
@@ -0,0 +1,20 @@
+// $(document).ready(function(){
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color':'#0000FF',
+//     'height':'100px'
+//   });
+// });
+
+$(function(){
+  $('.box1').slideDown(function(){
+    $('.box1').css({
+      'background-color':'#0000FF',
+      'width':'200px',
+      'height':'100px'
+    }).slideUp();
+  });
+});
