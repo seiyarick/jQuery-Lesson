cf6c96e2d997934bb1dfde7141624b2e90c539e2
seiyarick <seiyahirota2@gmail.com>
2022-05-26 22:51:51 +0900

    jQueryマウスオーバー

--- app.js
+++ app.js
@@ -9,12 +9,29 @@
 //   });
 // });
 
+// $(function(){
+//   $('.box1').slideDown(function(){
+//     $('.box1').css({
+//       'background-color':'#0000FF',
+//       'width':'200px',
+//       'height':'100px'
+//     }).slideUp();
+//   });
+// });
+
+// ----------------------
+
+// $('.セレクタ名').イベント名(function(){
+//   イベント発生時に行われる処理
+// });
+
 $(function(){
-  $('.box1').slideDown(function(){
-    $('.box1').css({
-      'background-color':'#0000FF',
-      'width':'200px',
-      'height':'100px'
-    }).slideUp();
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color':'#0000ff'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color':'#ff0000'});
   });
 });
+
+// -------------------------
